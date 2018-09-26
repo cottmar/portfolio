63077ad912f6cf068450133c04f7cf6bd2e408c8
cara ottmar <cara.ottmar@gmail.com>
2018-09-26 15:15:09 -0700

    header addition

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
+import { Header } from '../header/header';
+
 export default class App extends React.Component {
   render() {
     return (
@@ -9,8 +11,8 @@ export default class App extends React.Component {
         <Helmet>
           <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
           <meta property="og:title" content="Cara Ottmar : Portfolio"/>
-          <p> HJSDFHLSDJL </p>
         </Helmet>
+      <Header/>
       </BrowserRouter>
     );
   }
